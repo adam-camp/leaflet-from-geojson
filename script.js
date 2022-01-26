583267909b56c3bd31160665ad5f50093607975d
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-01-26 17:07:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,214 @@
-var map = L.map('mapid').setView([42, -88], 9);
+var map = L.map('mapid').setView([38.62,-90.99404], 9);
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+  // load a tile layer
+ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 18,
+	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
 
-var marker = L.marker([42,-88]).addTo(map);
-var marker_Camp = L.marker([41.8555,-88.15]).addTo(map);
-var marker_Bucci = L.marker([42.3,-88.4]).addTo(map);
 
-var circle = L.circle([41.975,-87.91], {
-    color: 'purple',
-    fillColor: '#f03',
-    fillOpacity: 0.75,
-    radius: 5000
-}).addTo(map);
-
-var polygon = L.polygon([
-    [42.509, -88.08],
-    [42.503, -88.06],
-    [42.51, -88.047]
-],
-{color:'green',
-fillColor:"yellow"}).addTo(map);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-marker_Camp.bindPopup("<b>Hello world!</b><br>I am the Camp House.").openPopup();
-marker_Bucci.bindPopup("<b>Hello world!</b><br>I am the Bucci House.").openPopup();
-circle.bindPopup("I am ORD Airport.");
-polygon.bindPopup("I am a polygon.");
-
-/*
-var popup = L.popup()
-    .setLatLng([41.5,-87.75])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
- */
-
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "House": "Camps",
+        "Address": "7727 Jerome Ave"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -90.32763719558716,
+          38.617708742410684
+        ]
+      }
+    }
+  ]
 }
 
-map.on('click', onMapClick);
+var feat = L.geoJSON(geojsonFeature).addTo(map);
+
+feat.bindPopup("<b>Hello world! I'm the Camp's 1st  house</b><br>7727 Jerome Ave.").openPopup();
+
+var myLines = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "RiverName": "Missouri"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -90.54725646972656,
+            38.681757748501546
+          ],
+          [
+            -90.53146362304688,
+            38.700515838688716
+          ],
+          [
+            -90.53558349609375,
+            38.72730457751627
+          ],
+          [
+            -90.50262451171875,
+            38.74605072069108
+          ],
+          [
+            -90.4833984375,
+            38.76586272164814
+          ],
+          [
+            -90.47584533691406,
+            38.78352824456894
+          ],
+          [
+            -90.4566192626953,
+            38.80868067783546
+          ],
+          [
+            -90.44975280761719,
+            38.82045110711473
+          ],
+          [
+            -90.43327331542967,
+            38.830614912420565
+          ],
+          [
+            -90.4119873046875,
+            38.829545106505996
+          ],
+          [
+            -90.3955078125,
+            38.8225909761771
+          ],
+          [
+            -90.37971496582031,
+            38.82205601494022
+          ],
+          [
+            -90.36117553710938,
+            38.8284752845139
+          ],
+          [
+            -90.34881591796875,
+            38.841846903808985
+          ],
+          [
+            -90.34263610839844,
+            38.853611850579966
+          ]
+        ]
+      }
+    }
+  ]
+}
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 2,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+var states = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Parks": "Forest Park",
+        "Has Baseball Field": "Yes"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -90.30057907104492,
+              38.64744452237617
+            ],
+            [
+              -90.30418395996094,
+              38.63370121904403
+            ],
+            [
+              -90.26461601257324,
+              38.628672522795796
+            ],
+            [
+              -90.26710510253906,
+              38.63323188898502
+            ],
+            [
+              -90.26556015014647,
+              38.644025702937036
+            ],
+            [
+              -90.30057907104492,
+              38.64744452237617
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Parks": "Tower Grove Park",
+        "Has Baseball Field": "No"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -90.24161338806152,
+              38.607128949679286
+            ],
+            [
+              -90.26818871498108,
+              38.60976146863579
+            ],
+            [
+              -90.26898264884949,
+              38.605125152362284
+            ],
+            [
+              -90.26677250862122,
+              38.605183841782065
+            ],
+            [
+              -90.24247169494629,
+              38.60286976543856
+            ],
+            [
+              -90.24161338806152,
+              38.607128949679286
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.["Has Baseball Field"]) {
+            case 'Yes': return {color: "#990000"};
+            case 'No':   return {color: "#003DA5"};
+        }
+    }
+}).addTo(map);
+
